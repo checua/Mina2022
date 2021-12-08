8ddf3847e81b1d6abe6d2e8ba7a1476a5c1a610a
Che <profesor76@hotmail.com>
2021-12-08 14:17:59 -0600

    7 Manifiesto Char Bar OK

--- app/src/main/java/com/example/base2021a/ManifiestoActivity.java
+++ app/src/main/java/com/example/base2021a/ManifiestoActivity.java
@@ -1,42 +1,153 @@
 package com.example.base2021a;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 
 public class ManifiestoActivity extends AppCompatActivity {
 
+    // variable for our bar chart
     BarChart barChart;
+
+    // variable for our bar data set.
+    BarDataSet barDataSet1, barDataSet2;
+
+    // array list for storing entries.
+    ArrayList barEntries;
+
+    // creating a string array for displaying days.
+    String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_manifiesto );
-        barChart = (BarChart) findViewById ( R.id.barChartM );
-        barChart.setDrawBarShadow ( false );
-        barChart.setDrawValueAboveBar ( false );
-        barChart.setMaxVisibleValueCount ( 50 );
-        barChart.setPinchZoom ( false );
-        barChart.setDrawGridBackground ( false);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manifiesto);
 
-        ArrayList<BarEntry> barEntries = new ArrayList<> (  );
-        barEntries.add ( new BarEntry ( 1, 40f ) );
-        barEntries.add ( new BarEntry ( 2, 44f ) );
-        barEntries.add ( new BarEntry ( 3, 30f ) );
-        barEntries.add ( new BarEntry ( 4, 36f ) );
+        // initializing variable for bar chart.
+        barChart = findViewById(R.id.barChartM);
 
-        BarDataSet barDataSet = new BarDataSet ( barEntries, "DataSet 1" );
-        barDataSet.setColors ( ColorTemplate.COLORFUL_COLORS );
-        BarData data = new BarData ( barDataSet);
-        data.setBarWidth (0.9f  );
-        barChart.setData ( data );
+        // creating a new bar data set.
+        barDataSet1 = new BarDataSet(getBarEntriesOne(), "First Set");
+        barDataSet1.setColor(getApplicationContext().getResources().getColor(R.color.purple_200));
+        barDataSet2 = new BarDataSet(getBarEntriesTwo(), "Second Set");
+        barDataSet2.setColor( Color.BLUE);
+
+        // below line is to add bar data set to our bar data.
+        BarData data = new BarData(barDataSet1, barDataSet2);
+
+        // after adding data to our bar data we
+        // are setting that data to our bar chart.
+        barChart.setData(data);
+
+        // below line is to remove description
+        // label of our bar chart.
+        barChart.getDescription().setEnabled(false);
+
+        // below line is to get x axis
+        // of our bar chart.
+        XAxis xAxis = barChart.getXAxis();
+
+        // below line is to set value formatter to our x-axis and
+        // we are adding our days to our x axis.
+        xAxis.setValueFormatter(new IndexAxisValueFormatter (days));
+
+        // below line is to set center axis
+        // labels to our bar chart.
+        xAxis.setCenterAxisLabels(true);
+
+        // below line is to set position
+        // to our x-axis to bottom.
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // below line is to set granularity
+        // to our x axis labels.
+        xAxis.setGranularity(1);
+
+        // below line is to enable
+        // granularity to our x axis.
+        xAxis.setGranularityEnabled(true);
+
+        // below line is to make our
+        // bar chart as draggable.
+        barChart.setDragEnabled(true);
+
+        // below line is to make visible
+        // range for our bar chart.
+        barChart.setVisibleXRangeMaximum(3);
+
+        // below line is to add bar
+        // space to our chart.
+        float barSpace = 0.1f;
+
+        // below line is use to add group
+        // spacing to our bar chart.
+        float groupSpace = 0.5f;
+
+        // we are setting width of
+        // bar in below line.
+        data.setBarWidth(0.15f);
+
+        // below line is to set minimum
+        // axis to our chart.
+        barChart.getXAxis().setAxisMinimum(0);
+
+        // below line is to
+        // animate our chart.
+        barChart.animate();
+
+        // below line is to group bars
+        // and add spacing to it.
+        barChart.groupBars(0, groupSpace, barSpace);
+
+        // below line is to invalidate
+        // our bar chart.
+        barChart.invalidate();
+    }
+
+    // array list for first set
+    private ArrayList<BarEntry> getBarEntriesOne() {
+
+        // creating a new array list
+        barEntries = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntries.add(new BarEntry(1f, 4));
+        barEntries.add(new BarEntry(2f, 6));
+        barEntries.add(new BarEntry(3f, 8));
+        barEntries.add(new BarEntry(4f, 2));
+        barEntries.add(new BarEntry(5f, 4));
+        barEntries.add(new BarEntry(6f, 1));
+        return barEntries;
+    }
+
+    // array list for second set.
+    private ArrayList<BarEntry> getBarEntriesTwo() {
+
+        // creating a new array list
+        barEntries = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntries.add(new BarEntry(1f, 8));
+        barEntries.add(new BarEntry(2f, 12));
+        barEntries.add(new BarEntry(3f, 4));
+        barEntries.add(new BarEntry(4f, 1));
+        barEntries.add(new BarEntry(5f, 7));
+        barEntries.add(new BarEntry(6f, 3));
+        return barEntries;
+    }
+
+        }
 
 
 
@@ -156,5 +267,4 @@ public class ManifiestoActivity extends AppCompatActivity {
     }
 
      */
-    }
-}
+
